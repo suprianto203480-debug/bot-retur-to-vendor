22dce9a13b97e32bbfc2491ee9c60589772b2d97
suprianto203480-debug <suprianto203480@gmail.com>
2026-03-05 22:05:33 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Bot Retur Vendor Aktif")
+
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    print("Bot berjalan...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
