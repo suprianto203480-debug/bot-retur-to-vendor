d402be8cfcf038e7ee49fae0ad9f0e76ae396e34
suprianto203480-debug <suprianto203480@gmail.com>
2026-03-06 20:13:38 +0700

    Update database.py

--- database.py
+++ database.py
@@ -2,4 +2,11 @@ import os
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(os.getenv("DATABASE_URL"))
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
+    conn = psycopg2.connect(
+        DATABASE_URL,
+        sslmode="require"
+    )
+
+    return conn
