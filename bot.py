6d166455cadc6a075608374ef908dfc63753d090
suprianto203480-debug <suprianto203480@gmail.com>
2026-03-12 10:08:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -84,10 +84,14 @@ def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.StatusUpdate.WEB_APP_DATA, webapp_handler))
 
-    print("✅ Bot scan barcode aktif")
+    app.add_handler(
+        MessageHandler(
+            filters.StatusUpdate.WEB_APP_DATA,
+            webapp_handler
+        )
+    )
+
+    print("✅ BOT SCANNER AKTIF")
     app.run_polling()
-
-if __name__ == "__main__":
     main()
